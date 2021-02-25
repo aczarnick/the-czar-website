bccc503ff9ef6c949a5ba28afcb928ff4935bce5
Alex Czarnick <apz2016@outlook.com>
2021-02-24 21:59:03 -0600

    removed all css references

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import './App.css';
 import Header from  './header/header'
 import Content from './content/content'
 import Footer from './footer/footer'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header/>
       <Content/>
       <Footer/>
-    </div>
+    </>
   );
 }
 
